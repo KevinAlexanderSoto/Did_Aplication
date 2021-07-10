e9972e2d3d304c98f2080e4d2442a37e24491f97
Kevin Soto <kevinalexandersoto999@gmail.com>
2021-07-10 10:14:37 -0500

    fits commit , bases del proyecto

---
+++ index.js
@@ -0,0 +1,15 @@
+require("colors");
+
+const principal = ()=>{
+
+    console.log('=======================');
+    console.log(`   ToDid ${'Aplication'.brightBlue.underline}`);
+    console.log('=======================');
+
+    console.log('\n 1. Anotar Tarea');
+
+
+}
+
+
+principal();
